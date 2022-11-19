c5037ca97af3680227abe13ee9432fc368da3c70
Rushil <86391663+MK1014@users.noreply.github.com>
2022-11-19 11:47:09 +0800

    Create ex-1.cpp

---
+++ ex-1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (void){
+    printf( "hello  Rushil  \n ");
+    printf( "my height is %d m %d cm \n",1,80 );
+    printf( " this building is %5d cm \n",890);
+    printf( " this building is %5d cm \n",7890);
+    printf( " this building is %5d cm \n",67890);
+    printf( " this building is %5d cm \n",567890);
+    printf( " this building is %5d cm \n",1234567890);
+    printf( " pi = %0.2f \n",3.14159265);
+}
