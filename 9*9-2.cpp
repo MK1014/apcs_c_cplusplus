a088cab0a3c3493d4218ae61736f861e6e00ecd3
Rushil <a0970571228@gmail.com>
2022-11-20 08:14:02 +0000

    9*9_for_loop換行

---
+++ 9*9-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i,j;
+    for(i=1;i<10;i++){
+        for(j=1;j<10;j++){
+            printf(" %d * %d = %d ",i,j,i*j);
+        }
+        printf("\n");
+    }
+    cout<<"Hello World";
+ 
+    return 0;
+}
