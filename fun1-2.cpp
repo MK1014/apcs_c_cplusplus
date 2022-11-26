5b50988e5fc2804089fcf8083d5d2beed52516a5
Rushil <a0970571228@gmail.com>
2022-11-26 02:09:23 +0000

    函數值

---
+++ fun1-2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int fun1(int a,int b){
+    return a+b;
+}
+int fun2(int a,int b){
+    return a-b;
+}
+int fun3(int a,int b){
+    return a*b;
+}
+int fun4(int a,int b){
+    return a/b;
+}
+void print_data(int a,int b){
+    int res;
+    res = fun1(a,b);
+    printf("aa = %d bb = %d aa + bb = %d \n",a,b,res);
+}
+int main(){
+    int aa = 30,bb = 60,res;
+    scanf("%d %d",&aa,&bb);
+    res = fun1(aa,bb);
+    printf("aa = %d bb = %d aa + bb = %d \n",aa,bb,res);
+    res = fun2(aa,bb);
+    printf("aa = %d bb = %d aa - bb = %d \n",aa,bb,res);
+    res = fun3(aa,bb);
+    printf("aa = %d bb = %d aa * bb = %d \n",aa,bb,res);
+    res = fun4(aa,bb);
+    printf("aa = %d bb = %d aa / bb = %d \n",aa,bb,res);
+    //print_data(aa,bb);
+    return 0;
+}
