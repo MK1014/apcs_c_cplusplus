8d5626e3c9604cc096c9f17e6355d9c66cdbb7c4
Rushil <86391663+MK1014@users.noreply.github.com>
2022-12-04 09:37:14 +0800

    分數平均計算與check 是否及格

---
+++ Score calculation.cpp
@@ -0,0 +1,38 @@
+
+#include <iostream>
+
+using namespace std;
+
+float print_sum(int score[],int n){
+    float f_ret;
+    int  i, sum=0;
+    for(i=0;i<n;i++){
+        //print("%p ",&score[i]);
+        sum += score[i];
+    }
+    f_ret = sum / n;
+    return f_ret;
+}
+
+int main(void)
+{
+    int n=5,i;
+    int score[5];
+    int u_c = 0;
+    float aver;
+    printf("please input 5 score:\n");
+    for(i=0;i<n;i++){
+        scanf("%d",&score[i]);
+        if(score[i]<60){
+            u_c++;
+        }
+    }
+    aver = print_sum(score,n);
+    printf("\nscore average = %f\n",aver);
+    if(u_c == 0){
+        printf("ALL pass!\n");
+    }else{
+        printf("need to put more attentions , have %d courses\n",u_c);
+    }
+    return 0;
+}
